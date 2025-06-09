8acece3d68ee2033e55e294f2d7b37eadced2d6f
Yaroslav Zaklynskyi <yaroslavz@wix.com>
2025-06-09 23:30:23 +0300

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'expo',
+    '@typescript-eslint/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+}; 
